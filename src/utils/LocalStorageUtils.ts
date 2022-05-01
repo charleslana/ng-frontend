c6375c364a53854fd294d19eddf50c64594c17f0
Charles <charleslanop@gmail.com>
2022-05-01 01:36:45 -0300

    updated i18n

--- src/utils/LocalStorageUtils.ts
+++ src/utils/LocalStorageUtils.ts
@@ -5,7 +5,7 @@ class LocalStorageUtils {
         return localStorage.getItem(this.locale) ?? window.navigator.language;
     }
 
-    public static setLocale(string: string) {
+    public static setLocale(string: string): void {
         localStorage.setItem(this.locale, string);
     }
 }
